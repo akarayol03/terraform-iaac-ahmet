3a600a182dd46448a5eddd7ec6f9839f965eef61
akarayol03 <akarayol03@gmail.com>
2020-01-08 19:46:52 -0600

    tower added

---
+++ data_source/tower.tf
@@ -0,0 +1,65 @@
+#Go to region 1'
+provider "aws" {
+    region = "us-east-1" 
+
+} 
+# Search for centos Latest with the owner
+data "aws_ami" "centos" {
+     filter { 
+         name = "root-device-type" 
+         values = ["ebs"] 
+         }
+         
+        filter { 
+            name   = "name" 
+            values = ["CentOS Linux 7 x86_64 HVM EBS *"] 
+            } 
+
+    most_recent = true
+    owners = ["679593333241"]
+
+} 
+#Show the AMI id
+output "ami" {
+    value = data.aws_ami.centos.id 
+
+} 
+
+resource "aws_key_pair" "towerkey" { 
+  key_name   = "towerkey" 
+  public_key = file("~/.ssh/id_rsa.pub") 
+} 
+
+
+
+
+
+resource "aws_instance" "web" {
+    ami           = data.aws_ami.ubuntu.id
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.towerkey.key_name
+    provisioner "remote-exec" { 
+        connection { 
+            host        = "self.public_ip" 
+            type        = "ssh" 
+            user        = "$centos" 
+            private_key = file("~/.ssh/id_rsa") 
+            } 
+            inline = [ 
+                "sudo yum install -y epel-release", 
+                ] 
+
+  } 
+tags = {
+  Name = "HelloWorld
+    } 
+}
+
+
+resource "aws_route53_record" "tower" { 
+  zone_id = "Z339ZQTAJHGFCO" 
+  name    = "tower.example.com" 
+  type    = "A" 
+  ttl     = "300" 
+  records = [aws.aws_instance.web.public_ip] 
+} 
