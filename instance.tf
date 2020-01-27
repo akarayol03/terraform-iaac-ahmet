d0891a873bcb547941a3c8a074456a0933f32cd8
akarayol03 <akarayol03@gmail.com>
2020-01-26 19:41:28 -0600

    added

--- instance.tf
+++ instance.tf
@@ -1,12 +1,7 @@
 resource "aws_instance" "web" { 
-    ami           = var.ami 
+    ami           = var.ami
     instance_type = var.instance_type
     associate_public_ip_address = var.associate_public_ip_address
-    key_name = var.key_name
-
-    security_groups = var.security_groups
-
-    user_data = var.user_data
 
 tags = { 
     Name = "TerraformTest" 
