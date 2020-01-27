375c4f60dccf86a436ee62beda76a26613b684e2
akarayol03 <akarayol03@gmail.com>
2020-01-26 20:13:15 -0600

    added

--- router53.tf
+++ router53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "www" { 
   zone_id = "Z339ZQTAJHGFCO" 
-  name    = "www.originalpeak.com" 
+  name    = "www.originalpeak.com." 
   type    = "A" 
   ttl     = "60"
   records = [aws_instance.web[0].public_ip] 
