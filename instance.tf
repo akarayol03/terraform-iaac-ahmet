850c6bdf4b2976ba1b24ae3b507138c9732ff28b
akarayol03 <akarayol03@gmail.com>
2020-01-27 20:02:14 -0600

    added count

--- instance.tf
+++ instance.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" { 
     ami                          = var.ami
     instance_type                = var.instance_type
+    count                        = var.count_instance
     associate_public_ip_address  = var.associate_public_ip_address
     key_name                     = var.key_name
     security_groups              = var.security_groups              
